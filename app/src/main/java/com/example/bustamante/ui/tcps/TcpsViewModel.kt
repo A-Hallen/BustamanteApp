caf4c4156bbade1dd3b3dac862447c3875db2d76
hallen <hallen412120@gmail.com>
2023-09-30 03:51:47 -0400

    configuracion inicial

--- app/src/main/java/com/example/bustamante/ui/tcps/TcpsViewModel.kt
+++ app/src/main/java/com/example/bustamante/ui/tcps/TcpsViewModel.kt
@@ -1,10 +1,10 @@
-package com.example.bustamante.ui.slideshow
+package com.example.bustamante.ui.tcps
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class SlideshowViewModel : ViewModel() {
+class TcpsViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
         value = "This is slideshow Fragment"
